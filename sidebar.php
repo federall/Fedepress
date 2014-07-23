e934e1553d93133feec033c0b2b1afd3190f7eaa
Benjamin Crozat <benjamincrozat@icloud.com>
2014-07-23 11:45:54 +0200

    Affichage de la sidebar si widgets

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,9 @@
-<div class="site-sidebar">
-  <?php dynamic_sidebar( 'sidebar' ); ?>
-</div><!-- /.site-sidebar -->
+<?php
+$id = 'sidebar';
+
+if ( is_active_sidebar( $id ) ) :
+?>
+  <div class="site-sidebar">
+    <?php dynamic_sidebar( $id ); ?>
+  </div><!-- /.site-sidebar -->
+<?php endif; ?>
