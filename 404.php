3d9b5f5397b3e383cd9dfad84b46d80b32a27c2c
Benjamin Crozat <benjamincrozat@icloud.com>
2014-07-29 11:16:31 +0200

    404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<article class="post">
+
+  <header class="post-header">
+    <h1><?php _e('Page non trouvée', 'fedepress'); ?></h1>
+  </header><!-- /.post-header -->
+
+  <div class="post-content">
+    <p><?php _e('Nous sommes désolés, la page que vous recherchez n\'existe pas.', 'fedepress'); ?></p>
+  </div><!-- /.post-content -->
+
+</article><!-- /.post -->
+
+<?php get_footer(); ?>
